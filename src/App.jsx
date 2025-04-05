82c12da01d2b2e0c7821084189203e7f8f838224
Vasil <vsilt2002@gmail.com>
2025-04-05 18:31:27 +0200

    Fixing export function default

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Logo from './assets/LogoQuiz.png';
 import Navbar from './components/Navbar';
 import CreateQuiz from './components/CreateQuiz';
+
 import './index.css';
 
 function App() {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Logo from './assets/LogoQuiz.png';
+import Logo from '../assets/LogoQuiz.png';
 function Navbar() {
-	<nav className="navbar navbar-light bg-light px-3">
-		<div className="container-fluid">
-			<div className="d-flex align-items-center">
-				<img
-					src={Logo}
-					alt="Logo"
-					className="img-fluid me-2"
-					style={{ width: '40px', height: '40px' }}
-				/>
-				<div className="navbar-brand d-flex flex-column">
-					<span className="fw-bold fs-6">Quizzes</span>
-					<span className="fw-bold text-muted fs-6">Professor</span>
+	return (
+		<nav className="navbar navbar-light bg-light px-3">
+			<div className="container-fluid">
+				<div className="d-flex align-items-center">
+					<img
+						src={Logo}
+						alt="Logo"
+						className="img-fluid me-2"
+						style={{ width: '40px', height: '40px' }}
+					/>
+					<div className="navbar-brand d-flex flex-column">
+						<span className="fw-bold fs-6">Quizzes</span>
+						<span className="fw-bold text-muted fs-6">Professor</span>
+					</div>
+				</div>
+
+				<div className="d-flex align-items-center ms-auto">
+					<img
+						src={Logo}
+						alt="Logo"
+						className="img-fluid me-2"
+						style={{ width: '40px', height: '40px' }}
+					/>
+					<i className="bi bi-person-circle fs-4"></i>
 				</div>
 			</div>
-
-			<div className="d-flex align-items-center ms-auto">
-				<img
-					src={Logo}
-					alt="Logo"
-					className="img-fluid me-2"
-					style={{ width: '40px', height: '40px' }}
-				/>
-				<i className="bi bi-person-circle fs-4"></i>
-			</div>
-		</div>
-	</nav>;
+		</nav>
+	);
 }
+export default Navbar;
