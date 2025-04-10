4d3d0c24b9abfa9543c6843b62ed22a780106186
Vasil <vsilt2002@gmail.com>
2025-04-10 22:22:57 +0200

    Added ProfileScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import CreateQuiz from "./components/CreateQuiz";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import CreateQuiz from './components/CreateQuiz';
 
-import "./index.css";
-import EditQuiz from "./components/EditQuiz";
-import QuizStartedV1 from "./components/QuizStartedV1";
-import Homepage from "./components/HomePage";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import './index.css';
+import EditQuiz from './components/EditQuiz';
+import QuizStartedV1 from './components/QuizStartedV1';
+import Homepage from './components/HomePage';
+import Login from './components/Login';
+import Register from './components/Register';
+import ProfileScreen from './components/ProfilePage';
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/profilescreen" element={<ProfileScreen />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
